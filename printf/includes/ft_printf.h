bee3d97abda2d026e44b32e6b614f4a763448256
Joao Oliveira <joaooliv@student.42.fr>
2022-09-23 19:19:03 +0100

    :lipstick: style - header files norminetadings

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joaooliv <joaooliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 16:17:08 by joaooliv          #+#    #+#             */
-/*   Updated: 2022/09/19 17:20:08 by joaooliv         ###   ########.fr       */
+/*   Updated: 2022/09/23 18:39:21 by joaooliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
 
 // BIG BOY
 int		ft_printf(const char *fmt, ...);
--- printf/includes/internals.h
+++ printf/includes/internals.h
@@ -6,25 +6,25 @@
 /*   By: joaooliv <joaooliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 16:14:25 by joaooliv          #+#    #+#             */
-/*   Updated: 2022/09/22 11:26:07 by joaooliv         ###   ########.fr       */
+/*   Updated: 2022/09/23 19:11:53 by joaooliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_FORMATS_H
-# define PRINTF_FORMATS_H
+#ifndef INTERNALS_H
+# define INTERNALS_H
 
-#include "printf.h"
-#include "../libft/libft.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdbool.h>
+# include "printf.h"
+# include "../libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdbool.h>
 
-#define PRINTF_FLAGS "#+-0 "
-#define FLAGS_N 5
+# define PRINTF_FLAGS "#+-0 "
+# define FLAGS_N 5
 
 // ordinal representation of the flags as they come up in the flags string
-typedef enum	e_flag
+typedef enum e_flag
 {
 	hash,
 	plus,
@@ -33,12 +33,13 @@ typedef enum	e_flag
 	space,
 }	t_flag;
 
-#define PRINTF_CONVERSIONS "cspdiuxX"
-#define CONVERSIONS_N 8
+# define PRINTF_CONVERSIONS "\%cspdiuxX"
+# define CONVERSIONS_N 9
 
 // ordinal representation of the convs as they come up in the convs string
-typedef enum	e_conversion
-{ 
+typedef enum e_conversion
+{
+	null,
 	character,
 	string,
 	pointer,
@@ -49,9 +50,6 @@ typedef enum	e_conversion
 	Hhex
 }	t_conversion;
 
-// t_flags is an array of booleans indicating if each flag is up or not
-typedef bool	t_flags[FLAGS_N];
-
 typedef struct s_f_options
 {
 	long			field_width;
@@ -59,22 +57,22 @@ typedef struct s_f_options
 	long			arg_i;
 	char			padd_char;
 	t_conversion	conv;
-	t_flags			flags;
+	bool			flags[FLAGS_N];
 }	t_f_options;
 
 // returns the index of c in charset, or -1 if not found
-long	charset_index(char c, char *charset);
+long		charset_index(char c, char *charset);
 
 // frees to_free and returns a NULL pointer
-void	*free_fail(void *to_free);
+void		*free_fail(void *to_free);
 
 // character to string
-char	*ctos(char c);
+char		*ctos(char c);
 
 // joins two strings and frees them
-char	*join_free(char *s1, char *s2);
+char		*join_free(char *s1, char *s2);
 
-void	sanitize_options(t_f_options *opts);
+void		sanitize_options(t_f_options *opts);
 
 t_f_options	*dup_options(t_f_options *opts);
 
@@ -83,15 +81,17 @@ t_f_options	*dup_options(t_f_options *opts);
 t_f_options	*parse_format(char *fmt_start, char **cont_fmt);
 
 // Converter functions
-//     These functions convert the next argument in the argument list to a string 
-//   in a specific format, based on the format options (f_options).
-char	*to_character(t_f_options *opts, va_list args);
-char	*to_str(t_f_options *opts, va_list args);
-char	*to_hex(t_f_options *opts, va_list args);
-char	*to_dec(t_f_options *opts, va_list args);
-char	*to_pointer(t_f_options *opts, va_list args);
+//     These functions convert the next argument in the argument 
+//   list to a string  in a specific format, based on the format
+//   options (f_options).
+char		*to_character(t_f_options *opts, va_list args);
+char		*to_str(t_f_options *opts, va_list args);
+char		*to_hex(t_f_options *opts, va_list args);
+char		*to_dec(t_f_options *opts, va_list args);
+char		*to_pointer(t_f_options *opts, va_list args);
+char		*to_perc(t_f_options *opts, va_list args);
 
 // size of a number in a given radix
-size_t	nbr_size(long long n, size_t radix);
+size_t		nbr_size(long long n, size_t radix);
 
 #endif
