df8fdabde5e3987ee2644eee0071d17ee5ecd8d9
joaooliv <joaooliv@student.42.fr>
2022-10-24 15:31:23 +0100

    test file, printf

--- printf/test_file.c
+++ printf/test_file.c
@@ -6,7 +6,7 @@
 /*   By: joaooliv <joaooliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:10:49 by joaooliv          #+#    #+#             */
-/*   Updated: 2022/09/23 19:13:43 by joaooliv         ###   ########.fr       */
+/*   Updated: 2022/09/27 13:59:19 by joaooliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdio.h>
 
 #define args 'A', "loha, camaradas da", 42 
-#define FMT "%c%010.4s %% '%3$+4..12d' (unsigned %3$+.2u, hex %3$#+.2x)\n"
+#define FMT "%c%010.4s \"%.3$%\"   \"%3$+4..12d\" (unsigned %3$+++.2u, hex %3$#+.2x)\n"
 
 int	main()
 {
