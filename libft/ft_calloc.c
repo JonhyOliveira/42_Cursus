f404dc653738ecfc7f68fbfa1002bdbff0a2aa4d
Joao Francisco Oliveira <joaooliv@42lisboa>
2022-11-02 17:34:03 +0000

    :bug: calloc now allocs 0 on size or nmemb 0

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: joaooliv <joaooliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 12:10:32 by joaooliv          #+#    #+#             */
-/*   Updated: 2022/10/26 15:48:55 by joaooliv         ###   ########.fr       */
+/*   Updated: 2022/11/02 17:29:33 by joaooliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,13 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void		*ret;
 	size_t		alloc_size;
 
-	ret = (void *) 0;
-	if (nmemb && nmemb <= INT_MAX / size)
-	{
-		alloc_size = nmemb * size;
-		ret = (void *)malloc(alloc_size);
-		if (ret)
-			ft_bzero(ret, alloc_size);
-	}
-	if (size == 0)
-		ret = (void *)malloc(0);
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	if (nmemb > INT_MAX / size)
+		return (0);
+	alloc_size = nmemb * size;
+	ret = (void *)malloc(alloc_size);
+	if (ret)
+		ft_bzero(ret, alloc_size);
 	return (ret);
 }
