ef37d04a49431b813a3c48b8eaa9726af0440756
Joao Francisco Oliveira <joaooliv@42lisboa>
2022-11-12 19:20:52 +0000

    get_next_line passing all testers :tada:

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaooliv <joaooliv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 15:25:14 by joaooliv          #+#    #+#             */
+/*   Updated: 2022/11/02 14:59:26 by joaooliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+
+typedef struct s_buffer
+{
+	char	*buf;
+	char	*it;
+	long	bytes_last_read;
+}	t_buffer;
+
+char	*get_next_line(int fd);
+
+#endif
